99311d03566c05b560cb36aa933948fbbd140744
scrpr <46473171+scrpr@users.noreply.github.com>
2025-07-14 16:42:30 +0800

    add build scripts

---
+++ build_scripts/build.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+import argparse
+from utils import FileManager, SubtitleProcessor
+
+def main(work_folder):
+    build_dir = os.path.join(work_folder, 'build')
+
+    FileManager.traverse_files(work_folder)
+
+    for root, dirs, files in os.walk(build_dir):
+        for file in files:
+            if file.endswith(".ass"):
+                input_path = os.path.join(root, file)
+                output_path = os.path.join(build_dir, "output", file)
+
+                # run templates
+                # if not file.startswith("op"):
+                print(f"Running templates on {input_path}")
+                if os.name == 'nt':  # Windows
+                    subprocess.run([f"{work_folder}/aegisub-cli/aegisub-cli.exe", "--automation", f"{work_folder}/aegisub-cli/automation/autoload/0x.KaraTemplater.moon", input_path, input_path, "0x539's Templater"])
+                else:
+                    subprocess.run(["wine", f"{work_folder}/aegisub-cli/aegisub-cli.exe", "--automation", f"{work_folder}/aegisub-cli/automation/autoload/0x.KaraTemplater.moon", input_path, input_path, "0x539's Templater"])
+
+    for root, dirs, files in os.walk(build_dir):
+        for file in files:
+            if file.endswith(".ass"):
+                input_path = os.path.join(root, file)
+                output_path = os.path.join(build_dir, "output", file)
+                if file.startswith("ep"):
+                    os.chdir(root)
+                    FileManager.merge_files(input_path, output_path)
+                    print(f"Merged {file}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("work_folder", help="Specify the work folder")
+    args = parser.parse_args()
+
+    main(args.work_folder)
---
+++ build_scripts/config.py
@@ -0,0 +1,14 @@
+font_replacements = {
+    "獅尾圓體SC-Bold": "獅尾圓體-Bold",
+    "思源黑体": "Source Han Sans TC",
+    "思源宋体": "Source Han Serif TC",
+    "Source Han Sans SC": "Source Han Sans TC",
+    "Source Han Serif SC": "Source Han Serif TC",
+    "仓耳周珂正大榜书": "悠哉字体",
+    "獅尾圓體SC-Black": "獅尾圓體-Black",
+    "方正VDL兆黑 简 SemiBold": "Sarasa Mono TC SemiBold",
+}
+
+user_protect_replace='悠哉字体\n仓耳周珂正大榜书'
+
+zhs_fontname = '筑紫A丸 SC'
